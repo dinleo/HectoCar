3c604bf8348e9bbab06882071f2dd0e1b27bb7b2
Desktop <dinleo11@gmail.com>
2025-05-21 04:46:38 +0900

    CE

--- solver/criterion/hecto_criterion.py
+++ solver/criterion/hecto_criterion.py
@@ -1,22 +1,19 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class HectoCriterion(nn.Module):
     def __init__(self, loss_weight=1.0):
         super().__init__()
         self.loss_weight = loss_weight
-        self.loss_fn = nn.BCEWithLogitsLoss()
+        self.loss_fn = nn.CrossEntropyLoss()
 
     def forward(self, outputs, progress=0):
-        pred_logits = outputs['pred_logits']           # (B, num_classes)
+        pred_logits = outputs['pred_logits']           # (B, num_classes), raw logits
         labels = [x['label'] for x in outputs['batched_input']]  # list of int
-        labels = torch.tensor(labels, dtype=torch.long, device=pred_logits.device)
+        labels = torch.tensor(labels, dtype=torch.long, device=pred_logits.device)  # (B,)
 
-        target = F.one_hot(labels, num_classes=pred_logits.shape[1]).float()  # (B, C)
-
-        loss = self.loss_fn(pred_logits, target)
+        loss = self.loss_fn(pred_logits, labels)
 
         return {
-            'bce_loss': loss * self.loss_weight
+            'ce_loss': loss * self.loss_weight
         }
